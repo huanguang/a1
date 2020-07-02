1d7c7867937dd0c4db60363583ebd14e8ecc295f
liuhuanguang <923414405@qq.com>
2020-07-03 01:04:54 +0800

    update

--- config/admin.php
+++ config/admin.php
@@ -400,10 +400,10 @@ return [
             // Set to `false` if you want to disable this extension
             'enable' => true,
             'tabs' => [
-                'base' => '基本设置',
-                'shop' => '店铺设置',
-                'uplaod' => '上传设置',
-                'image' => '' // if tab name is empty, get from trans : trans('admin.configx.tabs.image'); tab名称留空则从翻译中获取
+                'base' => '网站设置',
+//                'shop' => '店铺设置',
+//                'uplaod' => '上传设置',
+//                'image' => '' // if tab name is empty, get from trans : trans('admin.configx.tabs.image'); tab名称留空则从翻译中获取
             ],
             // Whether check group permissions.
             //if (!Admin::user()->can('confix.tab.base')) {/*hide base tab*/ } .
